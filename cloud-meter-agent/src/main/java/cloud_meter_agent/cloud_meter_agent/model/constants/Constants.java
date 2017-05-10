7a608b531a23b024317f8a92c7899ef54334c28d
liumingtong <liumingtong520@126.com>
2017-05-10 23:11:20 +0800

    update delete model.constants packge

--- cloud-meter-agent/src/main/java/cloud_meter_agent/cloud_meter_agent/model/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package cloud_meter_agent.cloud_meter_agent.model.constants;
-
-public interface Constants {
-	String WEBDRIVERPATH_CHROM_KEY="webdriver.chrome.driver";
-	String WEBDRIVERPATH_CHROM_VALUE="D:/home/soft/webdriver/chrome/chromedriver.exe";
-	String WEBDRIVERPATH_FIREFOX_KEY="webdriver.firefox.bin";
-	String WEBDRIVERPATH_FIREFOX_VALUE="C:/Program Files/Mozilla Firefox/firefox.exe";
-	//D:/home/soft/webdriver/firefox/geckodriver.exe";
-}
